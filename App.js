972704643d53441792b0f7ecd1d9053cb532b77c
Sathvik-Shetty-1569 <sathvikshetty189@gmail.com>
2025-07-07 00:12:21 +0530

    ODM included

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-import fastify from 'fastify'
+import fastify from 'fastify';
+import { connectDB } from './config/config.js';
+import 'dotenv/config';
 
 const app = fastify()
 
-app.get('/hello', async (request, reply) => {
-  return { message: 'Hello from Fastify!' }
+app.get('/', async(request, reply) => {
+  reply.send('Hello world')
 })
+app.get('/api', async(request, reply) => {
+  return { message: 'Welcome to the API' }
+})
+app.get('/about', (request, reply) => {
+  reply.send({ message: 'About page' });
+})
+app.get('/hello/:name', (request, reply) => {
+  const { name } = request.params;
+  reply.send({ greeting: `Hello, ${name}!` });
+});
 
-app.listen({ port: 3000 }, (err, address) => {
+
+const start = async () => {
+
+  try {
+  await connectDB(process.env.MONGO_URI);
+  app.listen({port: 3000}, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
-  console.log(`🚀 Server running at ${address}`)
+  console.log(`Server listening at ${address}`)
 })
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+start();
